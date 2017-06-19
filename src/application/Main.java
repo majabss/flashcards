cf66df602ca99065c617d4069b0587f910365bd3
Maja <mbrinkm4@gmail.com>
2017-06-19 22:03:00 +0200

    FXML Main erstellt

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,11 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import kartei.Karteikasten;
 import kartei.Karteiverwaltung;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
@@ -13,19 +15,22 @@ public class Main extends Application {
 	
 	@Override
 	public void init() {
-		//kk mit Datei initialisiseren
+		// kk mit Datei initialisiseren
 	}
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			// Szene 1
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			// Main
+			Parent main = FXMLLoader.load(getClass().getResource("main.fxml"));	         
+	        Scene smain = new Scene(main);
+	        
+			// BorderPane root = new BorderPane();
+			// Scene scene = new Scene(root,400,400);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			
 			// Hauptbühne setzen (Szene 1)
 			primaryStage.setTitle("Flashcards");
-			primaryStage.setScene(scene);
+			primaryStage.setScene(smain);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();

a1b153e16283c8a843f760da0429c7d560fc54d9
Maja <mbrinkm4@gmail.com>
2017-06-19 19:23:01 +0200

    Aufbau der GUI

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,17 +2,29 @@ package application;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
+import kartei.Karteikasten;
+import kartei.Karteiverwaltung;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	private Karteiverwaltung kk; 
+	
+	@Override
+	public void init() {
+		//kk mit Datei initialisiseren
+	}
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			// Szene 1
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			// Hauptbühne setzen (Szene 1)
+			primaryStage.setTitle("Flashcards");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
